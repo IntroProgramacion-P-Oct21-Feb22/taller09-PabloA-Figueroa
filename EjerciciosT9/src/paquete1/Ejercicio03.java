9442fe09f3eb7da2b153e276466b5d0d2015fc85
Pablo Figueroa <apfigueroa3@utpl.edu.ec>
2021-12-17 11:40:09 -0500

    Segundo Commit

--- EjerciciosT9/src/paquete1/Ejercicio03.java
+++ EjerciciosT9/src/paquete1/Ejercicio03.java
@@ -17,9 +17,33 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        int elementosArribaMedia;
-        int elementosAbajoMedia;
-        
+        int elementosArribaMedia = 0;
+        int elementosAbajoMedia = 0;
+        double suma = 0;
+        double media;
+        int i;
+
+        for (i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+        media = suma / i;
+        for (i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] > media) {
+                elementosArribaMedia = elementosArribaMedia + 1;
+            } else {
+                if (arreglo[i] < media) {
+                    elementosAbajoMedia = elementosAbajoMedia + 1;
+                }
+            }
+        }
+
+        System.out.printf("Elementos abajo de la media %s - "
+                + "Elementos sobe de la media %d\n",
+                elementosAbajoMedia,
+                elementosArribaMedia);
+        System.out.println("media: " + media);
+        System.out.println("suma: " + suma);
+
     }
 
 }
